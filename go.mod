906e0b05093192dab33b4ed4489dc075b0f92b3f
Fadlun Anaturdasa <f.anaturdasa@gmail.com>
2022-04-30 11:10:16 +0700

    feat: swagger integration

--- go.mod
+++ go.mod
@@ -3,7 +3,16 @@ module golang.api
 go 1.13
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/gin-gonic/gin v1.7.7
+	github.com/go-openapi/spec v0.20.6 // indirect
+	github.com/go-openapi/swag v0.21.1 // indirect
 	github.com/jinzhu/gorm v1.9.16
 	github.com/joho/godotenv v1.4.0
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/swaggo/gin-swagger v1.4.3
+	github.com/swaggo/swag v1.8.1
+	github.com/urfave/cli/v2 v2.5.1 // indirect
+	golang.org/x/sys v0.0.0-20220429233432-b5fbb4746d32 // indirect
+	golang.org/x/tools v0.1.10 // indirect
 )
--- main.go
+++ main.go
@@ -6,7 +6,10 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 	"golang.api/controllers"
+	"golang.api/docs"
 	"golang.api/models"
 )
 
@@ -18,6 +21,12 @@ func LoadEnvy() {
 	}
 }
 
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 func main() {
 	LoadEnvy()
 
@@ -36,12 +45,26 @@ func main() {
 		})
 	})
 
-	r.GET("/users", controllers.GetAllData)
-	r.GET("/users/:id", controllers.GetOneData)
-	r.POST("/users", controllers.CreateData)
-	r.PUT("/users/:id", controllers.UpdateData)
-	r.DELETE("/users/:id", controllers.DeleteData)
+	v1 := r.Group("/api/v1")
+	{
+		users := v1.Group("/users")
+		{
+			users.GET("/", controllers.GetAllData)
+			users.GET("/:id", controllers.GetOneData)
+			users.POST("/", controllers.CreateData)
+			users.PUT("/:id", controllers.UpdateData)
+			users.DELETE("/:id", controllers.DeleteData)
+		}
+	}
 
-	// host := "localhost"
+	docs.SwaggerInfo.Title = "Swagger Example API"
+	docs.SwaggerInfo.Description = "This is a sample server Petstore server."
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "petstore.swagger.io"
+	docs.SwaggerInfo.BasePath = "/v2"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	r.Run("localhost:" + os.Getenv("PORT"))
 }
