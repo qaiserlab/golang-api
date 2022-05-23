f13dc40233f577722c25ff20e3b60ac27002daa8
Fadlun Anaturdasa <f.anaturdasa@gmail.com>
2022-05-23 07:43:26 +0700

    feat: passing userinfo data from middleware

--- controllers/auth/auth.go
+++ controllers/auth/auth.go
@@ -25,7 +25,7 @@ import (
 func Login(c *gin.Context) {
 	var formData LoginForm
 	var user models.User
-	var loginResponse AuthResponse
+	var authResponse AuthResponse
 
 	JWT_KEY := []byte(os.Getenv("JWT_KEY"))
 	JWT_EXP, err := time.ParseDuration(os.Getenv("JWT_EXP"))
@@ -83,10 +83,10 @@ func Login(c *gin.Context) {
 	domain := os.Getenv("HOST") + ":" + os.Getenv("PORT")
 	c.SetCookie("token", tokenValue, int(expirationTime.Unix()), "/", domain, true, true)
 
-	loginResponse.AccessToken = tokenValue
-	loginResponse.RefreshToken = tokenValue
+	authResponse.AccessToken = tokenValue
+	authResponse.RefreshToken = tokenValue
 
-	c.JSON(http.StatusOK, gin.H{"data": loginResponse})
+	c.JSON(http.StatusOK, gin.H{"data": authResponse})
 }
 
 // Refresh       	godoc
@@ -98,44 +98,7 @@ func Login(c *gin.Context) {
 // @Success      	200 {object} AuthResponse
 // @Router       	/v1/auth/refresh [get]
 func Refresh(c *gin.Context) {
-	// var user models.User
-	// var loginResponse AuthResponse
-
-	// JWT_KEY := []byte(os.Getenv("JWT_KEY"))
-	// JWT_EXP, err := time.ParseDuration(os.Getenv("JWT_EXP"))
-
-	// expirationTime := time.Now().Add(JWT_EXP)
-
-	// claims := &Claims{
-	// 	FirstName:   user.FirstName,
-	// 	LastName:    user.LastName,
-	// 	Gender:      user.Gender,
-	// 	Email:       user.Email,
-	// 	PhoneNumber: user.PhoneNumber,
-	// 	Username:    user.Username,
-	// 	Role:        user.Role,
-	// 	StandardClaims: jwt.StandardClaims{
-	// 		// In JWT, the expiry time is expressed as unix milliseconds
-	// 		ExpiresAt: expirationTime.Unix(),
-	// 	},
-	// }
-
-	// token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	// tokenValue, err := token.SignedString(JWT_KEY)
-
-	// if err != nil {
-	// 	c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
-	// 	return
-	// }
-
-	// domain := os.Getenv("HOST") + ":" + os.Getenv("PORT")
-	// c.SetCookie("token", tokenValue, 10, "/", domain, true, true)
-
-	// loginResponse.AccessToken = tokenValue
-	// loginResponse.RefreshToken = tokenValue
-
-	// c.JSON(http.StatusOK, gin.H{"data": loginResponse})
+	// userInfo := c.MustGet("userInfo").(Claims)
 	userInfo := c.MustGet("userInfo")
-
 	c.JSON(http.StatusOK, gin.H{"data": userInfo})
 }
